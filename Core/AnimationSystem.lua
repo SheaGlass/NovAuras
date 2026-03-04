f52076499b3d40b93f26e2d8937700c57a54a2b1
SheaGlass <sheaglass91@gmail.com>
2026-03-04 12:57:06 +0000

    feat: add fade and pulse animation helpers

--- Core/AnimationSystem.lua
+++ Core/AnimationSystem.lua
@@ -1,1 +1,46 @@
--- NovAuras: AnimationSystem stub
+-- Core/AnimationSystem.lua
+NovAuras = NovAuras or {}
+NovAuras.AnimationSystem = {}
+
+-- Fade in a frame over `duration` seconds
+function NovAuras.AnimationSystem.FadeIn(frame, duration)
+    duration = duration or 0.3
+    local ag = frame:CreateAnimationGroup()
+    local fade = ag:CreateAnimation("Alpha")
+    fade:SetFromAlpha(0)
+    fade:SetToAlpha(1)
+    fade:SetDuration(duration)
+    ag:Play()
+    return ag
+end
+
+-- Fade out a frame over `duration` seconds, then call callback if provided
+function NovAuras.AnimationSystem.FadeOut(frame, duration, callback)
+    duration = duration or 0.3
+    local ag = frame:CreateAnimationGroup()
+    local fade = ag:CreateAnimation("Alpha")
+    fade:SetFromAlpha(1)
+    fade:SetToAlpha(0)
+    fade:SetDuration(duration)
+    if callback then
+        ag:SetScript("OnFinished", callback)
+    end
+    ag:Play()
+    return ag
+end
+
+-- Scale pulse: grow then shrink (entry animation)
+function NovAuras.AnimationSystem.Pulse(frame, duration)
+    duration = duration or 0.2
+    local ag = frame:CreateAnimationGroup()
+    local scaleUp = ag:CreateAnimation("Scale")
+    scaleUp:SetScale(1.3, 1.3)
+    scaleUp:SetDuration(duration / 2)
+    scaleUp:SetOrder(1)
+    local scaleDown = ag:CreateAnimation("Scale")
+    scaleDown:SetScale(1 / 1.3, 1 / 1.3)
+    scaleDown:SetDuration(duration / 2)
+    scaleDown:SetOrder(2)
+    ag:Play()
+    return ag
+end
